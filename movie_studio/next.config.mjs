a066fc06e401b7927c1f7c72e53370c5fb63c8fa
jgtech1205 <jgtech1205@gmail.com>
2025-05-29 09:45:43 -0400

    updated config file

--- movie_studio/next.config.mjs
+++ movie_studio/next.config.mjs
@@ -5,12 +5,22 @@ const nextConfig = {
       {
         protocol: "http",
         hostname: "image.tmdb.org",
+        pathname: "/**", // ensure all images from this host are allowed
       },
       {
         protocol: "https",
         hostname: "i.ibb.co",
+        pathname: "/**", // ensure all images from this host are allowed
       },
     ],
+  },
+  eslint: {
+    // Ignore ESLint build errors during production deployment
+    ignoreDuringBuilds: false,
+  },
+  typescript: {
+    // Set to false if you want the build to fail on TS errors
+    ignoreBuildErrors: false,
   },
 };
 
